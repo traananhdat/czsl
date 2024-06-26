a5819a2ae47578ba3d0fcc341cc1b2926a38d78d
traananhdat <160739786+traananhdat@users.noreply.github.com>
2024-06-26 22:37:24 +0700

    Update download_data.sh

--- utils/download_data.sh
+++ utils/download_data.sh
@@ -18,11 +18,11 @@ cp utils/download_embeddings.py $FOLDER/fast
 
 
 # Download everything
-wget --show-progress -O $FOLDER/attr-ops-data.tar.gz https://utexas.box.com/shared/static/h7ckk2gx5ykw53h8o641no8rdyi7185g.gz
+# wget --show-progress -O $FOLDER/attr-ops-data.tar.gz https://utexas.box.com/shared/static/h7ckk2gx5ykw53h8o641no8rdyi7185g.gz
 wget --show-progress -O $FOLDER/mitstates.zip http://wednesday.csail.mit.edu/joseph_result/state_and_transformation/release_dataset.zip
-wget --show-progress -O $FOLDER/utzap.zip http://vision.cs.utexas.edu/projects/finegrained/utzap50k/ut-zap50k-images.zip
-wget --show-progress -O $FOLDER/splits.tar.gz https://www.senthilpurushwalkam.com/publication/compositional/compositional_split_natural.tar.gz
-wget --show-progress -O $FOLDER/cgqa.zip https://s3.mlcloud.uni-tuebingen.de/czsl/cgqa-updated.zip
+# wget --show-progress -O $FOLDER/utzap.zip http://vision.cs.utexas.edu/projects/finegrained/utzap50k/ut-zap50k-images.zip
+# wget --show-progress -O $FOLDER/splits.tar.gz https://www.senthilpurushwalkam.com/publication/compositional/compositional_split_natural.tar.gz
+# wget --show-progress -O $FOLDER/cgqa.zip https://s3.mlcloud.uni-tuebingen.de/czsl/cgqa-updated.zip
 
 echo "Data downloaded. Extracting files..."
 sed -i "s|ROOT_FOLDER|$FOLDER|g" utils/reorganize_utzap.py
@@ -41,11 +41,11 @@ rm -r mit-states/release_dataset
 rename "s/ /_/g" mit-states/images/*
 
 # UT-Zappos50k
-unzip utzap.zip -d ut-zap50k/
-mv ut-zap50k/ut-zap50k-images ut-zap50k/_images/
+# unzip utzap.zip -d ut-zap50k/
+# mv ut-zap50k/ut-zap50k-images ut-zap50k/_images/
 
 # C-GQA
-unzip cgqa.zip -d cgqa/
+# unzip cgqa.zip -d cgqa/
 
 # Download new splits for Purushwalkam et. al
 tar -zxvf splits.tar.gz
